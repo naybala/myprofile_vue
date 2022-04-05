845a81983d7173a5c096ca358a082290bf2bb20d
naybala <naybalaismylife.nbl@gmail.com>
2022-04-05 18:19:55 +0630

    commit

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,13 @@
   <link rel="stylesheet" href="../public/fontawesome/css/all.css">
  <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<Loader/>
-<Header/>
-<Home/>
-<About/>
-<Skill/>
-<Project/>
-<Contact/>
-<Footer/>
+      <Header/>
+      <Home/>
+      <About/>
+      <Skill/>
+      <Project/>
+      <Contact/>
+      <Footer/>
 </template>
 
 <script>
